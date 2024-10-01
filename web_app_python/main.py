199fe34bf341dc57ddb8dd68faadfd24439a2075
Mamadou Kaba <mamadou.d.kaba@gmail.com>
2024-10-01 17:21:40 -0400

    Update main.py

--- web_app_python/main.py
+++ web_app_python/main.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 
 from google.cloud import bigquery
 
-@app.route("/", methods=['GET', 'POST'])
-def hello_world():
+
+
+@app.route("/user", methods=['GET'])
+def user_resource():
     """Example Hello World route."""
 
-    return f"Hello World!!!!!!"
+    return json.dumps({'id': 123, "name: "Mamadou"})
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
