cefb43d40c48f35e5122ece8fa3d12d35abbbeea
Mamadou Kaba <mamadou.d.kaba@gmail.com>
2024-10-01 17:28:37 -0400

    Update main.py

--- web_app_python/main.py
+++ web_app_python/main.py
@@ -1,6 +1,6 @@
 import os, json, time
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -16,7 +16,7 @@ def hello_world():
 def user_resource():
     """Example Hello World route."""
 
-    return json.dumps({'id': 123, "name: "Mamadou"})
+    return jsonify({'id': 123, "name: "Mamadou"})
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
